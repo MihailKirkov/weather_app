31a12141f11e96aeecf3bf4a99d48a0572a649bb
Mihail Kirkov <131696445+MihailKirkov@users.noreply.github.com>
2023-10-15 16:40:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from weather import main as get_weather
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    if request.method == 'POST':
+        print('post')
+        city = request.form['cityName']
+        state = request.form['stateName']
+        country = request.form['countryName']
+        if city and state and country:
+            data = get_weather(city, state, country)
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=False) # change to false when deploying
